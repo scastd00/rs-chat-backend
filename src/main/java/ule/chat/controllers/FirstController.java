2de021677e47ec55657406daab0501b81838e154
scastd00 <scastd00@estudiantes.unileon.es>
2022-06-11 01:23:56 +0200

    Removed unused imports

--- src/main/java/ule/chat/controllers/FirstController.java
+++ src/main/java/ule/chat/controllers/FirstController.java
@@ -1,6 +1,5 @@
 package ule.chat.controllers;
 
-import com.google.gson.reflect.TypeToken;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ule.chat.utils.Utils;
 
-import java.util.HashMap;
 import java.util.Map;
-
-import static ule.chat.utils.Constants.gson;
 
 @Slf4j
 @RestController
