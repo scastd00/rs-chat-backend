05d331ba564ad242eaccd1b8255a8c788c75ffba
scastd00 <scastd00@estudiantes.unileon.es>
2022-06-10 03:36:41 +0200

    Initial commit

---
+++ src/main/java/ule/chat/UleChatApplication.java
@@ -0,0 +1,11 @@
+package ule.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UleChatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UleChatApplication.class, args);
+	}
+}
---
+++ src/test/java/ule/chat/UleChatApplicationTests.java
@@ -0,0 +1,12 @@
+package ule.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UleChatApplicationTests {
+	@Test
+	void contextLoads() {
+		//
+	}
+}
