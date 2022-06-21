1637cde546f9ad72b44ffd5a3a96838e682b31ce
scastd00 <scastd00@estudiantes.unileon.es>
2022-06-21 21:45:15 +0200

    Added support for the backend running a JS process

--- main_backend.js
+++ main_backend.js
@@ -1,1 +1,14 @@
-// java -jar target/ule-chat-0.0.1.jar
+const { spawn } = require('child_process');
+
+// Load environment variables to process.env
+require('dotenv').config();
+
+const server = spawn('java', ['-jar', 'target/ule-chat-0.0.1.jar']);
+
+server.stdout.on("data", (data) => {
+  console.log(data.toString());
+});
+
+server.on("close", (code) => {
+  console.log(`Server exited with code ${code}`)
+});
