21654ca9a9e369c9c3f049dde5302de8e0d77182
scastd00 <scastd00@estudiantes.unileon.es>
2022-06-22 20:39:16 +0200

    Registry supports CORS from all origins. DTH

--- src/main/java/ule/chat/security/CorsConfig.java
+++ src/main/java/ule/chat/security/CorsConfig.java
@@ -13,8 +13,7 @@ public class CorsConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(@NotNull CorsRegistry registry) {
-				registry.addMapping("/**")
-				        .allowedOrigins("http://127.0.0.1");
+				registry.addMapping("/**");
 			}
 		};
 	}
