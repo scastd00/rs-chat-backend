bbbf6dc803edad10580cc57da45251fd80c0edc2
scastd00 <scastd00@estudiantes.unileon.es>
2022-06-22 21:39:43 +0200

    Log port to know it. DTH

--- src/main/react/src/pages/Login.jsx
+++ src/main/react/src/pages/Login.jsx
@@ -26,7 +26,10 @@ function Login() {
               password: data.get('password'),
             })
             .then((res) => console.log(res))
-            .catch((err) => console.log(err));
+            .catch((err) => {
+              console.log(err);
+              console.log(process.env.PORT);
+            });
   };
 
   return (
