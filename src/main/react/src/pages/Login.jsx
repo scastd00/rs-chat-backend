b5fe0bce2d179c68fc019d11704d0f3cd50d27b6
scastd00 <scastd00@estudiantes.unileon.es>
2022-06-22 02:11:00 +0200

    Formatting. DTH

--- src/main/react/src/pages/Login.jsx
+++ src/main/react/src/pages/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
               password: data.get('password'),
             })
             .then((res) => console.log(res))
-            .catch((err) => console.log(err.message));
+            .catch((err) => console.log(err));
   };
 
   return (
