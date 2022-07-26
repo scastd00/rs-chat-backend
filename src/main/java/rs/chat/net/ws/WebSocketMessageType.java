f23b4f1db2c27d6e634aa347880bef0501f92a9b
scastd00 <scastd00@estudiantes.unileon.es>
2022-07-27 00:25:26 +0200

    Message creation in Utils, and added a new type of message (SERVER_INFO)

--- src/main/java/rs/chat/net/ws/WebSocketMessageType.java
+++ src/main/java/rs/chat/net/ws/WebSocketMessageType.java
@@ -1,15 +1,15 @@
 package rs.chat.net.ws;
 
 public final class WebSocketMessageType {
-	public static final String NEW_USER = "NEW_USER";
-	public static final String END_CONNECTION = "END_CONNECTION";
-	public static final String USER_JOINED = "USER_JOINED";
+	public static final String USER_CONNECTED = "USER_CONNECTED";
+	public static final String USER_DISCONNECTED = "USER_DISCONNECTED";
 	public static final String TEXT_MESSAGE = "TEXT_MESSAGE";
 	public static final String IMAGE_MESSAGE = "IMAGE_MESSAGE";
 	public static final String AUDIO_MESSAGE = "AUDIO_MESSAGE";
 	public static final String VIDEO_MESSAGE = "VIDEO_MESSAGE";
-	public static final String KEEP_ALIVE_MESSAGE = "KEEP_ALIVE";
 	public static final String ACTIVE_USERS_MESSAGE = "ACTIVE_USERS";
+	public static final String SERVER_INFO_MESSAGE = "SERVER_INFO";
 
-	private WebSocketMessageType() {}
+	private WebSocketMessageType() {
+	}
 }
--- src/main/java/rs/chat/utils/Utils.java
+++ src/main/java/rs/chat/utils/Utils.java
@@ -1,7 +1,9 @@
 package rs.chat.utils;
 
 import com.auth0.jwt.JWT;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.reflect.TypeToken;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +11,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import static rs.chat.net.ws.WebSocketMessageType.SERVER_INFO_MESSAGE;
 import static rs.chat.utils.Constants.ALGORITHM;
 import static rs.chat.utils.Constants.GSON;
 
@@ -46,5 +49,29 @@ public final class Utils {
 
 	public static String shortJsonString(String key, String value) {
 		return GSON.toJson(Map.of(key, value));
+	}
+
+	public static String createServerMessage(String message) {
+		return createMessage("Server", "ALL", -1, SERVER_INFO_MESSAGE, null, "UTF-8", message);
+	}
+
+	public static String createMessage(String username, String chatId, long sessionId, String type,
+	                                   String token, String encoding, String content) {
+		JsonObject headers = new JsonObject();
+		headers.add("username", new JsonPrimitive(username));
+		headers.add("chatId", new JsonPrimitive(chatId));
+		headers.add("sessionId", new JsonPrimitive(sessionId));
+		headers.add("type", new JsonPrimitive(type));
+		headers.add("token", token == null ? JsonNull.INSTANCE : new JsonPrimitive(token));
+
+		JsonObject body = new JsonObject();
+		body.add("encoding", new JsonPrimitive(encoding));
+		body.add("content", new JsonPrimitive(content));
+
+		JsonObject fullMessage = new JsonObject();
+		fullMessage.add("headers", headers);
+		fullMessage.add("body", body);
+
+		return fullMessage.toString();
 	}
 }
