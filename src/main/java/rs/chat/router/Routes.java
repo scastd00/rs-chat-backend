7689e1965645c7b1fe6fa4616a5e1a4f6445bd97
scastd00 <scastd00@estudiantes.unileon.es>
2022-08-13 23:15:01 +0200

    Changed name of prefix in routes

--- src/main/java/rs/chat/router/Routes.java
+++ src/main/java/rs/chat/router/Routes.java
@@ -4,10 +4,10 @@ public class Routes {
 	private Routes() {
 	}
 
-	private static final String API_URL_PREFIX = "/api/v1";
+	private static final String V_1 = "/api/v1";
 	public static final String ROOT_URL = "/";
 
-	public static final String REFRESH_TOKEN_URL = API_URL_PREFIX + "/token/refresh";
+	public static final String REFRESH_TOKEN_URL = V_1 + "/token/refresh";
 	public static final String WS_CHAT_ENDPOINT = "/ws/rschat";
 
 	public static class GetRoute {
@@ -16,10 +16,10 @@ public class Routes {
 		private GetRoute() {
 		}
 
-		public static final String USERS_URL = API_URL_PREFIX + "/users";
-		public static final String OPENED_SESSIONS_OF_USER_URL = API_URL_PREFIX + "/sessions/{username}";
-		public static final String DEGREES_URL = API_URL_PREFIX + "/degrees";
-		public static final String DEGREE_BY_NAME_URL = API_URL_PREFIX + "/degree/{degreeName}";
+		public static final String USERS_URL = V_1 + "/users";
+		public static final String OPENED_SESSIONS_OF_USER_URL = V_1 + "/sessions/{username}";
+		public static final String DEGREES_URL = V_1 + "/degrees";
+		public static final String DEGREE_BY_NAME_URL = V_1 + "/degree/{degreeName}";
 
 		public String[] lowTierRoutes() {
 			return new String[] {
@@ -46,12 +46,12 @@ public class Routes {
 		private PostRoute() {
 		}
 
-		public static final String LOGIN_URL = API_URL_PREFIX + "/login";
-		public static final String LOGOUT_URL = API_URL_PREFIX + "/logout";
-		public static final String REGISTER_URL = API_URL_PREFIX + "/register";
+		public static final String LOGIN_URL = V_1 + "/login";
+		public static final String LOGOUT_URL = V_1 + "/logout";
+		public static final String REGISTER_URL = V_1 + "/register";
 
-		public static final String USER_SAVE_URL = API_URL_PREFIX + "/user/save";
-		public static final String DEGREE_SAVE_URL = API_URL_PREFIX + "/degree/save";
+		public static final String USER_SAVE_URL = V_1 + "/user/save";
+		public static final String DEGREE_SAVE_URL = V_1 + "/degree/save";
 
 		public String[] lowTierRoutes() {
 			return new String[] {
@@ -78,8 +78,8 @@ public class Routes {
 		private PutRoute() {
 		}
 
-		public static final String CHANGE_PASSWORD_URL = API_URL_PREFIX + "/changePassword/{username}";
-		public static final String EDIT_DEGREE_NAME_URL = API_URL_PREFIX + "/degree/editName";
+		public static final String CHANGE_PASSWORD_URL = V_1 + "/changePassword/{username}";
+		public static final String EDIT_DEGREE_NAME_URL = V_1 + "/degree/editName";
 
 		public String[] lowTierRoutes() {
 			return new String[] {
