b1909c2b8a601a452a9b4dabf94cc716cde3240e
scastd00 <scastd00@estudiantes.unileon.es>
2022-08-25 20:15:23 +0200

    Added javadoc comments to storage package

--- src/main/java/rs/chat/storage/S3.java
+++ src/main/java/rs/chat/storage/S3.java
@@ -1,11 +1,8 @@
 package rs.chat.storage;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import rs.chat.net.ws.WSMessage;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.awscore.exception.AwsServiceException;
-import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
@@ -29,11 +26,17 @@ import static rs.chat.utils.Constants.REMOTE_S3_ENDPOINT_URI;
 import static rs.chat.utils.Constants.S3_BUCKET_NAME;
 import static rs.chat.utils.Utils.isDevEnv;
 
+/**
+ * Class that provides utility methods to work with S3.
+ */
 @Slf4j
 public class S3 {
 	private static final S3 INSTANCE = new S3();
 	private final S3Client s3Client;
 
+	/**
+	 * Private constructor that initializes the S3 client.
+	 */
 	private S3() {
 		this.s3Client = S3Client.builder()
 		                        .endpointOverride(isDevEnv() ?
@@ -44,10 +47,16 @@ public class S3 {
 		                        .build();
 	}
 
+	/**
+	 * @return the singleton instance of the S3 class
+	 */
 	public static S3 getInstance() {
 		return INSTANCE;
 	}
 
+	/**
+	 * @return the credentials for the S3 client
+	 */
 	private AwsBasicCredentials obtainCredentials() {
 		return AwsBasicCredentials.create(
 				System.getenv("AWS_ACCESS_KEY_ID"),
@@ -55,6 +64,11 @@ public class S3 {
 		);
 	}
 
+	/**
+	 * Checks if the S3 bucket exists.
+	 *
+	 * @throws SdkException if the bucket does not exist or there is an error.
+	 */
 	public void checkS3BucketConnectivity() {
 		HeadBucketResponse headBucketResponse = this.s3Client.headBucket(
 				HeadBucketRequest.builder()
@@ -72,6 +86,12 @@ public class S3 {
 		}
 	}
 
+	/**
+	 * Uploads a chat file to S3 bucket.
+	 *
+	 * @param chatId      the chat id of the chat file to upload to S3 bucket.
+	 * @param messageType the message type of the chat file to upload to S3 bucket.
+	 */
 	public void uploadFile(String chatId, WSMessage messageType) {
 		File file = messageType.buildFileInDisk(chatId);
 		String s3Key = messageType.s3Key(chatId);
@@ -92,7 +112,16 @@ public class S3 {
 		}
 	}
 
-	@SneakyThrows(AwsServiceException.class)
+	/**
+	 * Downloads a chat file from S3 bucket.
+	 *
+	 * @param chatId      the chat id of the chat file to download from S3 bucket.
+	 * @param messageType the message type of the chat file to download from S3 bucket.
+	 *
+	 * @return the downloaded chat file.
+	 *
+	 * @throws SdkException if there is an error with the S3 client.
+	 */
 	public File downloadFile(String chatId, WSMessage messageType) {
 		File file = messageType.buildFileInDisk(chatId);
 		String s3Key = messageType.s3Key(chatId);
@@ -110,6 +139,13 @@ public class S3 {
 		return file;
 	}
 
+	/**
+	 * Checks if the key exists in the S3 bucket.
+	 *
+	 * @param s3Key the key to check.
+	 *
+	 * @return true if the key exists in the S3 bucket, false otherwise.
+	 */
 	private boolean existsKeyInBucket(String s3Key) {
 		HeadObjectResponse response;
 
@@ -122,8 +158,6 @@ public class S3 {
 			);
 		} catch (NoSuchKeyException ignored) {
 			return false;
-		} catch (AwsServiceException | SdkClientException e) {
-			throw new RuntimeException(e);
 		}
 
 		return response.sdkHttpResponse().isSuccessful();
