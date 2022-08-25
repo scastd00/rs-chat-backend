366a6dcbb8e139e57bad796847548e3ef9bed855
scastd00 <scastd00@estudiantes.unileon.es>
2022-08-25 19:47:02 +0200

    Added javadoc comments to policies package

--- src/main/java/rs/chat/policies/Policies.java
+++ src/main/java/rs/chat/policies/Policies.java
@@ -2,7 +2,6 @@ package rs.chat.policies;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import rs.chat.exceptions.BadRequestException;
 import rs.chat.exceptions.InvalidPasswordException;
 import rs.chat.exceptions.MaliciousCodeInjectionException;
 import rs.chat.exceptions.MinimumRequirementsNotMetException;
@@ -22,6 +21,15 @@ public final class Policies {
 		excludedCharacters.add("=");
 	}
 
+	/**
+	 * Checks if the given user contains valid fields to register a new user.
+	 *
+	 * @param body the body of the request that contains the user.
+	 *
+	 * @throws MinimumRequirementsNotMetException if the user does not contain all the necessary fields or they
+	 *                                            are incorrect.
+	 * @throws MaliciousCodeInjectionException    if the username or passwords contains malicious code.
+	 */
 	public static void checkRegister(JsonObject body) {
 		String username = get(body, "username").getAsString().trim();
 		String fullName = get(body, "fullName").getAsString().trim();
@@ -31,15 +39,15 @@ public final class Policies {
 		boolean agreeTerms = get(body, "agreeTerms").getAsBoolean();
 
 		if (!agreeTerms) {
-			throw new BadRequestException("You must accept the terms and conditions before using the app.");
+			throw new MinimumRequirementsNotMetException("You must accept the terms and conditions before using the app.");
 		}
 
 		if (username.length() < 5 || username.length() > 15) {
-			throw new BadRequestException("Username must contain between 5 and 15 characters.");
+			throw new MinimumRequirementsNotMetException("Username must contain between 5 and 15 characters.");
 		}
 
 		if (fullName.length() > 100) {
-			throw new BadRequestException("Full name must be shorter than 100 characters.");
+			throw new MinimumRequirementsNotMetException("Full name must be shorter than 100 characters.");
 		}
 
 		if (containsSQLKeywords(fullName)) {
@@ -47,7 +55,7 @@ public final class Policies {
 		}
 
 		if (!email.matches("^[^@]+@[^@]+\\.[^@]{2,}$")) {
-			throw new BadRequestException("Email must have a valid structure. Eg: hello@domain.com");
+			throw new MinimumRequirementsNotMetException("Email must have a valid structure. Eg: hello@domain.com");
 		}
 
 		if (containsSQLKeywords(password)) {
@@ -57,13 +65,22 @@ public final class Policies {
 		checkPasswords(password, confirmPassword);
 	}
 
+	/**
+	 * Checks if the passwords are valid and if they match.
+	 *
+	 * @param password        the password of the user.
+	 * @param confirmPassword the password confirmation.
+	 *
+	 * @throws MinimumRequirementsNotMetException if the password is too short or too long.
+	 * @throws InvalidPasswordException           if the passwords don't match.
+	 */
 	private static void checkPasswords(String password, String confirmPassword) {
 		if (password.length() < 8) {
-			throw new InvalidPasswordException("Password is too simple, please provide 8 or more characters (up to 28).");
+			throw new MinimumRequirementsNotMetException("Password is too simple, please provide 8 or more characters (up to 28).");
 		}
 
 		if (password.length() > 28) {
-			throw new InvalidPasswordException("Password is too long. It must be between 8 and 28 characters.");
+			throw new MinimumRequirementsNotMetException("Password is too long. It must be between 8 and 28 characters.");
 		}
 
 		if (!password.equals(confirmPassword)) {
@@ -71,10 +88,27 @@ public final class Policies {
 		}
 	}
 
-	private static boolean containsSQLKeywords(String password) {
-		return excludedCharacters.stream().anyMatch(password::contains);
+	/**
+	 * Checks if the {@link String} contains any SQL keywords.
+	 *
+	 * @param s the {@link String} to check.
+	 *
+	 * @return true if the {@link String} contains any SQL keywords, false otherwise.
+	 */
+	private static boolean containsSQLKeywords(String s) {
+		return excludedCharacters.stream().anyMatch(s::contains);
 	}
 
+	/**
+	 * Retrieves the value of the given key from the given {@link JsonObject} object.
+	 *
+	 * @param json the {@link JsonObject} object to retrieve the value from.
+	 * @param key  the key of the value to retrieve.
+	 *
+	 * @return the value of the given key from the given {@link JsonObject} object.
+	 *
+	 * @throws MinimumRequirementsNotMetException if the given key is not found in the given {@link JsonObject} object.
+	 */
 	private static JsonElement get(JsonObject json, String key) {
 		if (!json.has(key)) {
 			throw new MinimumRequirementsNotMetException(key + " is not present in the request body.");
@@ -83,6 +117,11 @@ public final class Policies {
 		return json.get(key);
 	}
 
+	/**
+	 * Checks if the given user contains valid passwords to change them.
+	 *
+	 * @param body the body of the request that contains the user.
+	 */
 	public static void checkPasswords(JsonObject body) {
 		String newPassword = get(body, "newPassword").getAsString().trim();
 		String confirmPassword = get(body, "confirmPassword").getAsString().trim();
