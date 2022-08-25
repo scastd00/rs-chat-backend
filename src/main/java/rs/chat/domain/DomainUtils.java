4d8f6f613baf14d5322b1dbd3f447173312c0dee
scastd00 <scastd00@estudiantes.unileon.es>
2022-08-25 19:27:34 +0200

    Added javadoc comments to domain package

--- src/main/java/rs/chat/domain/DomainUtils.java
+++ src/main/java/rs/chat/domain/DomainUtils.java
@@ -17,29 +17,66 @@ public final class DomainUtils {
 	private DomainUtils() {
 	}
 
+	/**
+	 * Instantiates a new {@link Chat} object with the given parameters.
+	 *
+	 * @param name         the name of the chat.
+	 * @param chatType     the type of the chat.
+	 * @param s3ChatPrefix the prefix of the chat folder in S3.
+	 *
+	 * @return the new {@link Chat} object.
+	 */
 	@NotNull
-	private static Chat createChat(String name, String chatType, String chatPrefix) {
+	private static Chat createChat(String name, String chatType, String s3ChatPrefix) {
 		return new Chat(
 				null,
 				name,
 				chatType,
-				chatPrefix + name,
+				s3ChatPrefix + name,
 				Utils.jsonOfNumber("createdAt", System.currentTimeMillis())
 		);
 	}
 
+	/**
+	 * Returns a new {@link Chat} of <b>individual</b> type.
+	 *
+	 * @param name the name of the chat.
+	 *
+	 * @return the new {@link Chat} of <b>individual</b> type.
+	 */
 	public static Chat individualChat(String name) {
 		return createChat(name, USER_CHAT, USER_CHAT_S3_FOLDER_PREFIX);
 	}
 
+	/**
+	 * Returns a new {@link Chat} of <b>group</b> type.
+	 *
+	 * @param name the name of the chat.
+	 *
+	 * @return the new {@link Chat} of <b>group</b> type.
+	 */
 	public static Chat groupChat(String name) {
 		return createChat(name, GROUP_CHAT, GROUP_CHAT_S3_FOLDER_PREFIX);
 	}
 
+	/**
+	 * Returns a new {@link Chat} of <b>subject</b> type.
+	 *
+	 * @param name the name of the chat.
+	 *
+	 * @return the new {@link Chat} of <b>subject</b> type.
+	 */
 	public static Chat subjectChat(String name) {
 		return createChat(name, SUBJECT_CHAT, SUBJECT_CHAT_S3_FOLDER_PREFIX);
 	}
 
+	/**
+	 * Returns a new {@link Chat} of <b>degree</b> type.
+	 *
+	 * @param name the name of the chat.
+	 *
+	 * @return the new {@link Chat} of <b>degree</b> type.
+	 */
 	public static Chat degreeChat(String name) {
 		return createChat(name, DEGREE_CHAT, DEGREE_CHAT_S3_FOLDER_PREFIX);
 	}
