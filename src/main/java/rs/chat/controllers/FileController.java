abcc9e2bab40b11d41eecfe9b4553ae495a164d5
scastd00 <scastd00@estudiantes.unileon.es>
2022-08-28 21:03:18 +0200

    Added more metadata to file

--- src/main/java/rs/chat/controllers/FileController.java
+++ src/main/java/rs/chat/controllers/FileController.java
@@ -10,10 +10,14 @@ import rs.chat.net.http.HttpResponse;
 import rs.chat.net.http.HttpResponse.HttpResponseBody;
 import rs.chat.storage.S3;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -37,16 +41,41 @@ public class FileController {
 		                  .split(",")[1];
 		String fileName = body.get("name").getAsString();
 
+		byte[] fileBytes = Base64.getDecoder().decode(data);
+		BufferedImage image = ImageIO.read(new ByteArrayInputStream(fileBytes));
+
+		Map<String, String> metadata = new HashMap<>();
+		metadata.put("type", body.get("type").getAsString());
+		metadata.put("width", String.valueOf(image.getWidth()));
+		metadata.put("height", String.valueOf(image.getHeight()));
+		metadata.put("size", String.valueOf(fileBytes.length));
+		metadata.put("maxWidth", this.getMaxWidth(image.getWidth(), image.getHeight()));
+
 		URI uri = S3.getInstance().uploadImage(
 				URLEncoder.encode(fileName, UTF_8),
-				Base64.getDecoder().decode(data),
-				Map.of("type", body.get("type").getAsString())
+				fileBytes,
+				metadata
 		);
 
 		HttpResponseBody responseBody = new HttpResponseBody("uri", uri);
 		responseBody.add("name", fileName);
+		responseBody.add("metadata", metadata);
 
 		response.status(OK).send(responseBody);
 		log.info("File uploaded successfully");
+	}
+
+	private String getMaxWidth(int width, int height) {
+		if (width <= 200 || height <= 200) {
+			return "xs";
+		} else if (width <= 500 || height <= 500) {
+			return "sm";
+		} else if (width <= 800 || height <= 800) {
+			return "md";
+		} else if (width <= 1200 || height <= 1200) {
+			return "lg";
+		} else {
+			return "xl";
+		}
 	}
 }
