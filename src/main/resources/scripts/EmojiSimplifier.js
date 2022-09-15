a47dd8bc46f0d7da2cf084f8d9c8f01d4339cade
scastd00 <scastd00@estudiantes.unileon.es>
2022-09-15 13:46:11 +0200

    Reduced file of emojis to only necessary fields

---
+++ src/main/resources/scripts/EmojiSimplifier.js
@@ -0,0 +1,47 @@
+const emojis = require('../emojis.json');
+const fs = require('fs');
+
+const reducedEmojisFile = fs.openSync('./SimplifiedEmojis.min.json', 'w+');
+
+function takeNecessaryProperties(emoji) {
+	return {
+		id: emoji.id,
+		name: emoji.name,
+		emoji: emoji.emoji,
+		unicode: emoji.unicode,
+		category: emoji.category.name,
+		subcategory: emoji.sub_category.name,
+	};
+}
+
+const reducedEmojis = [];
+
+emojis.forEach(emoji => {
+	reducedEmojis.push(takeNecessaryProperties(emoji));
+
+	if (emoji.children) {
+		emoji.children.forEach(child => {
+			reducedEmojis.push(takeNecessaryProperties(child));
+		});
+	}
+});
+
+reducedEmojis.sort((a, b) => a.id - b.id);
+
+const maxUnicodeLength = reducedEmojis.reduce((max, emoji) => {
+    if (emoji.emoji.length > max) {
+	    console.log(emoji);
+        return emoji.emoji.length;
+    }
+    return max;
+}, 0);
+
+// Max name length = 80
+// Max emoji length = 15
+// Max unicode length = 54
+// Max category length = 17
+// Max subcategory length = 22
+
+fs.writeSync(reducedEmojisFile, JSON.stringify(reducedEmojis));
+
+console.log('Done!');
