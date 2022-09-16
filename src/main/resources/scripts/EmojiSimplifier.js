f04d47a74ace473017c0ae2c026b9a5237570a93
scastd00 <scastd00@estudiantes.unileon.es>
2022-09-16 17:41:16 +0200

    Replaced spaces, ', ' and ': ' from emojis. Removed formatted json of emojis

--- src/main/resources/scripts/EmojiSimplifier.js
+++ src/main/resources/scripts/EmojiSimplifier.js
@@ -1,4 +1,4 @@
-const emojis = require('../emojis.json');
+const emojis = require('../emojis.min.json');
 const fs = require('fs');
 
 const reducedEmojisFile = fs.openSync('./SimplifiedEmojis.min.json', 'w+');
@@ -6,7 +6,7 @@ const reducedEmojisFile = fs.openSync('./SimplifiedEmojis.min.json', 'w+');
 function takeNecessaryProperties(emoji) {
 	return {
 		id: emoji.id,
-		name: emoji.name,
+		name: emoji.name.replaceAll(': ', '+').replaceAll(', ', '&').replaceAll(' ', '_'),
 		icon: emoji.emoji,
 		unicode: emoji.unicode,
 		category: emoji.category.name,
@@ -28,13 +28,13 @@ emojis.forEach(emoji => {
 
 reducedEmojis.sort((a, b) => a.id - b.id);
 
-const maxUnicodeLength = reducedEmojis.reduce((max, emoji) => {
-	if (emoji.icon.length > max) {
-		console.log(emoji);
-		return emoji.icon.length;
-	}
-	return max;
-}, 0);
+// const maxUnicodeLength = reducedEmojis.reduce((max, emoji) => {
+// 	if (emoji.icon.length > max) {
+// 		console.log(emoji);
+// 		return emoji.icon.length;
+// 	}
+// 	return max;
+// }, 0);
 
 // Max name length = 80
 // Max emoji length = 15
@@ -46,23 +46,21 @@ fs.writeSync(reducedEmojisFile, JSON.stringify(reducedEmojis));
 
 console.log('Done!');
 
-//	private void saveAllEmojis() throws IOException {
-//		File file = new File("src/main/resources/scripts/SimplifiedEmojis.min.json");
-//		String s = IOUtils.toString(new FileReader(file));
-//		JsonArray emojis = new Gson().fromJson(s, JsonArray.class);
+//  File file = new File("src/main/resources/scripts/SimplifiedEmojis.min.json");
+//  String s = IOUtils.toString(new FileReader(file));
+//  JsonArray emojis = new Gson().fromJson(s, JsonArray.class);
 //
-//		for (JsonElement emoji : emojis) {
-//			JsonObject object = emoji.getAsJsonObject();
-//			String name = object.get("name").getAsString();
-//			String icon = object.get("icon").getAsString();
-//			String unicode = object.get("unicode").getAsString();
-//			String category = object.get("category").getAsString();
-//			String subcategory = object.get("subcategory").getAsString();
+//  for (JsonElement emoji : emojis) {
+//  	JsonObject object = emoji.getAsJsonObject();
+//  	String name = object.get("name").getAsString();
+//  	String icon = object.get("icon").getAsString();
+//  	String unicode = object.get("unicode").getAsString();
+//  	String category = object.get("category").getAsString();
+//  	String subcategory = object.get("subcategory").getAsString();
 //
-//			if (!this.emojiService.exists(name)) {
-//				this.emojiService.save(
-//						new Emoji(null, name, icon, unicode, category, subcategory)
-//				);
-//			}
-//		}
-//	}
+//  	if (!this.emojiService.exists(name)) {
+//  		this.emojiService.save(
+//  				new Emoji(null, name, icon, unicode, category, subcategory)
+//  		);
+//  	}
+//  }
