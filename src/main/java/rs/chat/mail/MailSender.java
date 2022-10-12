7fc2816a4646548c5800b718e0a1296d6f17b31e
scastd00 <scastd00@estudiantes.unileon.es>
2022-10-12 23:28:24 +0200

    Moved email to Gmail. Outlook sends mails to spam (don't know why)

--- src/main/java/rs/chat/mail/MailSender.java
+++ src/main/java/rs/chat/mail/MailSender.java
@@ -1,72 +1,90 @@
 package rs.chat.mail;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMailMessage;
 
+import javax.mail.Authenticator;
+import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
-import static rs.chat.utils.Constants.APPLICATION_EMAIL;
-
+@Slf4j
 public class MailSender {
-	private static final JavaMailSender sender;
-
 	private MailSender() {
 	}
 
+	private static final String FROM = System.getenv("GMAIL_ACCOUNT");
+	private static final String SMTP_HOST = "smtp.gmail.com";
+	private static final Properties PROPERTIES = System.getProperties();
+	private static final Session SESSION;
+
 	static {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		// Setup mail server
+		PROPERTIES.put("mail.smtp.host", SMTP_HOST);
+		PROPERTIES.put("mail.smtp.port", "465");
+		PROPERTIES.put("mail.smtp.auth", "true");
+		PROPERTIES.put("mail.smtp.ssl.enable", "true");
+		PROPERTIES.put("mail.smtp.socketFactory.port", "465");
+		PROPERTIES.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
-		mailSender.setHost("smtp-mail.outlook.com");
-		mailSender.setPort(587);
-		mailSender.setUsername(System.getenv("EMAIL_USERNAME"));
-		mailSender.setPassword(System.getenv("EMAIL_PASSWORD"));
+		// Get the Session object and pass username and password
+		SESSION = Session.getInstance(PROPERTIES, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(FROM, System.getenv("GMAIL_APP_PASSWORD"));
+			}
+		});
 
-		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-//		props.put("mail.debug", "true");
-
-		sender = mailSender;
+		// Used to debug SMTP issues
+		SESSION.setDebug(false);
 	}
 
 	public static void sendRegistrationEmail(String to, String username) {
 		MimeMessage message = getMimeMessage(to, "Welcome to RSChat!",
 		                                     "src/main/resources/templates/email/welcome/rs-chat-welcome-email.html",
 		                                     "{{username}}", username);
-		sender.send(message);
+		try {
+			Transport.send(message);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public static void resetPassword(String to, String code) {
 		MimeMessage message = getMimeMessage(to, "Reset your password",
 		                                     "src/main/resources/templates/email/resetPassword/rs-chat-reset-password-email.html",
 		                                     "{{code}}", code);
-		sender.send(message);
+		try {
+			Transport.send(message);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	private static MimeMessage getMimeMessage(String to, String subject, String fileName, String target, String value) {
-		MimeMailMessage message = new MimeMailMessage(sender.createMimeMessage());
-
 		try {
-			message.setFrom(APPLICATION_EMAIL);
-			message.setTo(to);
+			MimeMessage message = new MimeMessage(SESSION);
+
+			message.setFrom(new InternetAddress(FROM));
+			message.addRecipient(RecipientType.TO, new InternetAddress(to));
 			message.setSubject(subject);
-			message.getMimeMessage().setText(
+			message.setText(
 					IOUtils.toString(new FileReader(fileName)).replace(target, value),
 					StandardCharsets.UTF_8.name(),
 					"html"
 			);
+
+			return message;
 		} catch (MessagingException | IOException e) {
 			throw new RuntimeException(e);
 		}
-
-		return message.getMimeMessage();
 	}
 }
