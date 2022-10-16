ed97d9f7b1b2e70a477f3b8b6845c86dd609b6fc
scastd00 <scastd00@estudiantes.unileon.es>
2022-10-16 02:05:23 +0200

    Updated script to be able to deploy the app

--- exec.sh
+++ exec.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-run_jar() {
+run_application_jar() {
 	mvn clean package -DskipTests
-	java -jar target/rs-chat-backend-0.0.1.jar
+	java -jar target/rs-chat-backend-0.0.1.jar &>/dev/null &
 }
 
 export_env() {
-	# Read the .env file and export the variables
+	# Read the corresponding env file and export the variables
 	while read -r line; do
-		export "$line"
-	done < .env
+	export $line &> /dev/null
+done < .env."$1"
 }
 
 create_s3_bucket() {
@@ -23,6 +23,29 @@ create_s3_bucket() {
 	aws s3api head-bucket --bucket "$AWS_S3_BUCKET_NAME" 2> /dev/null || aws --endpoint-url=http://localhost:4566 s3 mb s3://"$AWS_S3_BUCKET_NAME"
 }
 
-export_env
-create_s3_bucket
-run_jar
+main() {
+	echo 'You are starting the application'
+	echo 'Possible commands:'
+	echo '  1) Deploy to development environment'
+	echo '  2) Deploy to production environment'
+
+	read -rp 'Enter the command number: ' command
+
+	case $command in
+		1)
+			export_env dev
+			create_s3_bucket
+			run_application_jar
+			;;
+		2)
+			export_env prod
+			create_s3_bucket
+			run_application_jar
+			;;
+		*)
+			echo 'Invalid command'
+			;;
+	esac
+}
+
+main
