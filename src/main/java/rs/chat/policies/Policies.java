4e0573da1b9a7bd98a400c2ec1c4c005a5e4c140
scastd00 <scastd00@estudiantes.unileon.es>
2023-01-20 23:42:12 +0100

    Improved code and added more constraints in Policies

--- src/main/java/rs/chat/policies/Policies.java
+++ src/main/java/rs/chat/policies/Policies.java
@@ -1,10 +1,11 @@
 package rs.chat.policies;
 
 import am.ik.yavi.builder.ValidatorBuilder;
-import am.ik.yavi.constraint.BooleanConstraint;
+import am.ik.yavi.constraint.CharSequenceConstraint;
 import am.ik.yavi.constraint.password.CharSequencePasswordPoliciesBuilder;
 import am.ik.yavi.core.ConstraintViolations;
 import am.ik.yavi.core.Validator;
+import am.ik.yavi.core.ViolationMessage;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import lombok.AccessLevel;
@@ -14,6 +15,7 @@ import rs.chat.exceptions.MaliciousCodeInjectionException;
 import rs.chat.exceptions.MinimumRequirementsNotMetException;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Policies {
@@ -26,13 +28,13 @@ public final class Policies {
 		excludedCharacters.add("=");
 	}
 
-	private static final String emailKey = "email";
-	private static final String usernameKey = "username";
-	private static final String fullNameKey = "fullName";
-	private static final String passwordKey = "password";
-	private static final String confirmPasswordKey = "confirmPassword";
-	private static final String agreeTermsKey = "agreeTerms";
-	private static final String equalPasswordsKey = "equalPasswords";
+	private static final String EMAIL_KEY = "email";
+	private static final String USERNAME_KEY = "username";
+	private static final String FULL_NAME_KEY = "fullName";
+	private static final String PASSWORD_KEY = "password";
+	private static final String CONFIRM_PASSWORD_KEY = "confirmPassword";
+	private static final String AGREE_TERMS_KEY = "agreeTerms";
+	private static final String EQUAL_PASSWORDS_KEY = "equalPasswords";
 
 	/**
 	 * Checks if the given user contains valid fields to register a new user.
@@ -44,24 +46,31 @@ public final class Policies {
 	 * @throws MaliciousCodeInjectionException    if the username or passwords contains malicious code.
 	 */
 	public static void checkRegister(JsonObject body) {
-		String email = get(body, emailKey).getAsString().trim();
-		String username = get(body, usernameKey).getAsString().trim();
-		String fullName = get(body, fullNameKey).getAsString().trim();
-		String password = get(body, passwordKey).getAsString().trim();
-		String confirmPassword = get(body, confirmPasswordKey).getAsString().trim();
-		boolean agreeTerms = get(body, agreeTermsKey).getAsBoolean();
-
 		Validator<JsonObject> validator = ValidatorBuilder
 				.<JsonObject>of()
-				._boolean(o -> agreeTerms, agreeTermsKey, BooleanConstraint::isTrue)
-				._string(o -> email, emailKey, c -> c.notBlank().email())
-				._string(o -> username, usernameKey, c -> c.notBlank().greaterThanOrEqual(5).lessThanOrEqual(15).pattern("^[a-zA-Z0-9_]+$"))
-				._string(o -> fullName, fullNameKey, c -> c.notBlank().lessThanOrEqual(100))
-				._string(o -> fullName, fullNameKey, c -> c.predicate(Policies::containsSQLKeywords, fullNameKey, "contains malicious code"))
-				._string(o -> password, passwordKey, c -> c.notBlank().password(CharSequencePasswordPoliciesBuilder::strong).greaterThanOrEqual(8).lessThanOrEqual(28))
-				._string(o -> password, passwordKey, c -> c.predicate(Policies::containsSQLKeywords, passwordKey, "contains malicious code"))
-				._string(o -> confirmPassword, confirmPasswordKey, c -> c.notBlank().greaterThanOrEqual(8).lessThanOrEqual(28))
-				._boolean(o -> password.equals(confirmPassword), equalPasswordsKey, BooleanConstraint::isTrue)
+				._boolean(o -> get(o, AGREE_TERMS_KEY).getAsBoolean(),
+				          AGREE_TERMS_KEY,
+				          c -> c.isTrue()
+				                .message("You must agree to the terms and conditions."))
+				.nest(o -> o, EMAIL_KEY, emailValidator())
+				._string(o -> get(o, USERNAME_KEY).getAsString().trim(),
+				         USERNAME_KEY,
+				         c -> c.notBlank()
+				               .greaterThanOrEqual(5)
+				               .lessThanOrEqual(15)
+				               .pattern("^[a-zA-Z0-9_]+$")
+				               .message("The username can only contain letters, numbers and underscores. Must be " +
+						                        "between 5 and 15 characters."))
+				._string(o -> get(o, FULL_NAME_KEY).getAsString().trim(),
+				         FULL_NAME_KEY,
+				         c -> c.notBlank()
+				               .lessThanOrEqual(100)
+				               .predicate(
+						               Predicate.not(Policies::containsSQLKeywords),
+						               ViolationMessage.of(FULL_NAME_KEY, "\"{0}\" contains malicious code"))
+				               .message("The full name can only contain letters, numbers and underscores. " +
+						                        "Must have less than 100 characters."))
+				.nest(o -> o, "passwords", passwordValidator())
 				.failFast(true)
 				.build();
 
@@ -71,47 +80,13 @@ public final class Policies {
 			return;
 		}
 
-		switch (violations.get(0).name()) {
-			case agreeTermsKey -> throw new MinimumRequirementsNotMetException("You must agree to the terms and conditions.");
-			case emailKey -> throw new MinimumRequirementsNotMetException("The email is invalid.");
-			case usernameKey -> throw new MinimumRequirementsNotMetException("The username is invalid.");
-			case fullNameKey -> throw new MinimumRequirementsNotMetException("The full name is invalid.");
-			case passwordKey -> throw new InvalidPasswordException("The password is invalid.");
-			case confirmPasswordKey -> throw new InvalidPasswordException("The confirm password is invalid.");
-			case equalPasswordsKey -> throw new InvalidPasswordException("The passwords do not match.");
-			default -> throw new MinimumRequirementsNotMetException("The request is invalid.");
-		}
-	}
+		String name = violations.get(0).name();
 
-	/**
-	 * Checks if the passwords are valid and if they match.
-	 *
-	 * @param password        the password of the user.
-	 * @param confirmPassword the password confirmation.
-	 *
-	 * @throws MinimumRequirementsNotMetException if the password is too short or too long.
-	 * @throws InvalidPasswordException           if the passwords don't match.
-	 */
-	private static void checkPasswords(String password, String confirmPassword) {
-		if (password.length() < 8) {
-			throw new MinimumRequirementsNotMetException("Password is too simple, please provide 8 or more characters (up to 28).");
+		if (name.equals(PASSWORD_KEY) || name.equals(CONFIRM_PASSWORD_KEY) || name.equals(EQUAL_PASSWORDS_KEY)) {
+			throw new InvalidPasswordException(violations.get(0).message());
 		}
 
-		if (password.length() > 28) {
-			throw new MinimumRequirementsNotMetException("Password is too long. It must be between 8 and 28 characters.");
-		}
-
-		if (!password.equals(confirmPassword)) {
-			throw new InvalidPasswordException("Passwords do not match.");
-		}
-	}
-
-	public static void passwordValidator(String password) {
-		ValidatorBuilder
-				.<String>of()
-				._string(s -> password, passwordKey, c -> c.notBlank().password(CharSequencePasswordPoliciesBuilder::strong).greaterThanOrEqual(8).lessThanOrEqual(28))
-				._string(s -> password, passwordKey, c -> c.predicate(Policies::containsSQLKeywords, passwordKey, "contains malicious code"))
-				.build();
+		throw new MinimumRequirementsNotMetException(violations.get(0).message());
 	}
 
 	/**
@@ -133,11 +108,11 @@ public final class Policies {
 	 *
 	 * @return the value of the given key from the given {@link JsonObject} object.
 	 *
-	 * @throws MinimumRequirementsNotMetException if the given key is not found in the given {@link JsonObject} object.
+	 * @throws IllegalArgumentException if the given key is not found in the given {@link JsonObject} object.
 	 */
 	private static JsonElement get(JsonObject json, String key) {
 		if (!json.has(key)) {
-			throw new MinimumRequirementsNotMetException(key + " is not present in the request body.");
+			throw new IllegalArgumentException(key + " is not present in the request body.");
 		}
 
 		return json.get(key);
@@ -147,19 +122,95 @@ public final class Policies {
 	 * Checks if the given user contains valid passwords to change them.
 	 *
 	 * @param body the body of the request that contains the user.
+	 *
+	 * @throws InvalidPasswordException           if the passwords don't match.
+	 * @throws MinimumRequirementsNotMetException if the password is too short or too long.
 	 */
 	public static void checkPasswords(JsonObject body) {
-		String newPassword = get(body, "newPassword").getAsString().trim();
-		String confirmPassword = get(body, confirmPasswordKey).getAsString().trim();
+		ConstraintViolations violations = passwordValidator().validate(body);
 
-		checkPasswords(newPassword, confirmPassword);
+		if (violations.isValid()) {
+			return;
+		}
+
+		String name = violations.get(0).name();
+
+		if (name.equals(EQUAL_PASSWORDS_KEY)) {
+			throw new InvalidPasswordException(violations.get(0).message());
+		}
+
+		throw new MinimumRequirementsNotMetException(violations.get(0).message());
 	}
 
+	/**
+	 * Checks if the email contained in the body is valid.
+	 *
+	 * @param body JsonObject containing the email.
+	 *
+	 * @throws MinimumRequirementsNotMetException if the email is not valid.
+	 */
 	public static void checkEmail(JsonObject body) {
-		String email = get(body, emailKey).getAsString().trim();
-
-		if (!email.matches("^[^@]+@[^@]+\\.[^@]{2,}$")) {
+		if (!emailValidator().validate(body).isValid()) {
 			throw new MinimumRequirementsNotMetException("Email must have a valid structure. Eg: hello@domain.com");
 		}
+	}
+
+	/**
+	 * Validator to check passwords follow the following rules:
+	 * <ul>
+	 *     <li>Must not be blank.</li>
+	 *     <li>Must be between 8 and 28 characters long.</li>
+	 *     <li>Must be a strong password (see {@link CharSequencePasswordPoliciesBuilder#strong()}).</li>
+	 *     <li>Must not contain any SQL keywords.</li>
+	 *     <li>Must be equal to the confirmation password.</li>
+	 * </ul>
+	 *
+	 * @return a {@link Validator} that validates the password, the confirmation password and the
+	 * equality of the two.
+	 */
+	private static Validator<JsonObject> passwordValidator() {
+		return ValidatorBuilder
+				.<JsonObject>of()
+				._string(o -> get(o, PASSWORD_KEY).getAsString().trim(),
+				         PASSWORD_KEY,
+				         c -> c.notBlank()
+				               .password(CharSequencePasswordPoliciesBuilder::strong)
+				               .greaterThanOrEqual(8)
+				               .lessThanOrEqual(28)
+				               .predicate(
+						               Predicate.not(Policies::containsSQLKeywords),
+						               ViolationMessage.of(PASSWORD_KEY, "\"{0}\" contains malicious code"))
+				               .message("The password must contain at least one uppercase letter, one lowercase " +
+						                        "letter, one number and one special character. It must be " +
+						                        "between 8 and 28 characters."))
+				._string(o -> get(o, CONFIRM_PASSWORD_KEY).getAsString().trim(),
+				         CONFIRM_PASSWORD_KEY,
+				         c -> c.notBlank()
+				               .greaterThanOrEqual(8)
+				               .lessThanOrEqual(28)
+				               .message("Confirmation password must have between 8 and 28 characters."))
+				._boolean(o -> {
+					          String password = get(o, PASSWORD_KEY).getAsString().trim();
+					          String confirmPassword = get(o, CONFIRM_PASSWORD_KEY).getAsString().trim();
+					          return password.equals(confirmPassword);
+				          },
+				          EQUAL_PASSWORDS_KEY,
+				          c -> c.isTrue()
+				                .message("Passwords do not match."))
+				.build();
+	}
+
+	/**
+	 * @return a {@link Validator} that checks if the email fulfills the rules specified in
+	 * {@link CharSequenceConstraint#email()}
+	 */
+	private static Validator<JsonObject> emailValidator() {
+		return ValidatorBuilder
+				.<JsonObject>of()
+				._string(o -> get(o, EMAIL_KEY).getAsString().trim(), EMAIL_KEY,
+				         c -> c.notBlank()
+				               .email()
+				               .message("The email is invalid."))
+				.build();
 	}
 }
