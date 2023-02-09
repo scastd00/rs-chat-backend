2aed34daffff80cc121b4739750bb92a55473b9a
scastd00 <scastd00@estudiantes.unileon.es>
2023-02-09 14:31:15 +0100

    Mentions with commands in the same message are sent only once

--- src/main/java/rs/chat/net/ws/strategies/messages/impl/ParseableMessageStrategy.java
+++ src/main/java/rs/chat/net/ws/strategies/messages/impl/ParseableMessageStrategy.java
@@ -19,6 +19,7 @@ import rs.chat.net.ws.strategies.messages.events.MentionMessageEvent;
 import rs.chat.utils.Utils;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Predicate;
 
 import static rs.chat.net.ws.Message.ERROR_MESSAGE;
@@ -40,42 +41,49 @@ public class ParseableMessageStrategy extends GenericMessageStrategy {
 	@Override
 	public void handle(MessageHandlingDTO handlingDTO) throws WebSocketException, IOException {
 		try {
+			// This reference allows to send the message content to other clients only once
+			AtomicBoolean messageContentSent = new AtomicBoolean(false);
+
 			MessageParser.parse(handlingDTO.wrappedMessage().content())
 			             .stream()
 			             .filter(Predicate.not(ParsedData::isText)) // Ignore messages
 			             .forEach(parsedData -> {
 				             switch (parsedData.type()) {
-					             case COMMAND -> runCommand(handlingDTO, parsedData);
-					             case MENTION -> sendMentionToUser(handlingDTO, parsedData);
+					             case COMMAND -> runCommand(handlingDTO, parsedData, messageContentSent);
+					             case MENTION -> sendMentionToUser(handlingDTO, parsedData, messageContentSent);
 					             default -> {/**/}
 				             }
 			             });
 		} catch (CommandUnavailableException | IllegalArgumentException e) {
 			log.error("Error while parsing message", e);
-			String errorMessage = Utils.createMessage(
-					"An error occurred while parsing the message, %s".formatted(e.getMessage()),
-					ERROR_MESSAGE.type(),
-					"NONE"
-			);
-			handlingDTO.getSession().sendMessage(new TextMessage(errorMessage));
+			handlingDTO.getSession().sendMessage(new TextMessage(
+					Utils.createMessage(
+							"An error occurred while parsing the message, %s".formatted(e.getMessage()),
+							ERROR_MESSAGE.type(),
+							"NONE"
+					)
+			));
 		}
 	}
 
 	/**
 	 * Sends a mention message to the user to notify them.
 	 *
-	 * @param handlingDTO DTO containing the required data.
-	 * @param parsedData  parsed data from the message (contains the mentioned username).
+	 * @param handlingDTO        DTO containing the required data.
+	 * @param parsedData         parsed data from the message (contains the mentioned username).
+	 * @param messageContentSent boolean to check if the message has already been sent.
 	 */
-	private void sendMentionToUser(MessageHandlingDTO handlingDTO, ParsedData parsedData) {
+	private void sendMentionToUser(MessageHandlingDTO handlingDTO, ParsedData parsedData, AtomicBoolean messageContentSent) {
 		// If the user is the same as the one who sent the message, ignore it
 		if (handlingDTO.wrappedMessage().username().equals(parsedData.data())) {
 			return;
 		}
 
 		try {
-			// Send the message to other clients
-			super.handle(handlingDTO);
+			// Send the message to other clients only if it hasn't been sent yet
+			if (!messageContentSent.getAndSet(true)) {
+				super.handle(handlingDTO);
+			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -100,17 +108,19 @@ public class ParseableMessageStrategy extends GenericMessageStrategy {
 	/**
 	 * Runs the command.
 	 *
-	 * @param handlingDTO DTO containing the required data.
-	 * @param parsedData  parsed data from the message (contains the command).
+	 * @param handlingDTO        DTO containing the required data.
+	 * @param parsedData         parsed data from the message (contains the command).
+	 * @param messageContentSent boolean to check if the message has already been sent.
 	 */
-	private void runCommand(MessageHandlingDTO handlingDTO, ParsedData parsedData) {
+	private void runCommand(MessageHandlingDTO handlingDTO, ParsedData parsedData, AtomicBoolean messageContentSent) {
 		Command command = CommandMappings.getCommand(parsedData.data());
 		handlingDTO.otherData().put("commandParams", parsedData.params());
 
 		try {
 			// If the command message could be sent to others, send it.
 			// If it is for the user only, don't send it.
-			if (command.sendToOthers()) {
+			// The message is only sent once, so if it has already been sent, don't send it again.
+			if (command.sendToOthers() && !messageContentSent.getAndSet(true)) {
 				super.handle(handlingDTO);
 			}
 
