624f07f446cb7ca4e5cf727a4dc8a0b558988e26
scastd00 <scastd00@estudiantes.unileon.es>
2023-04-05 04:06:44 +0200

    Fixed bug when updating user state in db on NSFW content upload
    NSFWMessageEventListener uses ReentrantLock to prevent several threads to update the db at the same time. In addition, all the sessions of the user are removed.

--- src/main/java/rs/chat/ai/events/NSFWMessageEventListener.java
+++ src/main/java/rs/chat/ai/events/NSFWMessageEventListener.java
@@ -7,11 +7,13 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import rs.chat.domain.entity.User;
+import rs.chat.domain.service.SessionService;
 import rs.chat.domain.service.UserService;
 import rs.chat.net.ws.strategies.messages.notifications.UserBlockedEvent;
 import rs.chat.observability.metrics.Metrics;
 
 import java.time.Clock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static rs.chat.utils.Constants.DEFAULT_BLOCK_DURATION_MILLIS;
 import static rs.chat.utils.Constants.MINIMUM_NSFW_TO_BLOCK;
@@ -24,10 +26,19 @@ public class NSFWMessageEventListener implements ApplicationListener<NSFWUploadE
 	private final Clock clock;
 	private final Metrics metrics;
 	private final ApplicationEventPublisher publisher;
+	private final SessionService sessionService;
+	private final ReentrantLock lock = new ReentrantLock();
 
 	@Override
 	public void onApplicationEvent(@NotNull NSFWUploadEvent event) {
+		this.lock.lock();
 		User user = this.userService.getUserById(event.getUserId());
+
+		if (user.getBlockUntil() != null && user.getBlockUntil().isAfter(this.clock.instant())) {
+			this.lock.unlock();
+			return;
+		}
+
 		user.setNsfwCount((byte) (user.getNsfwCount() + 1));
 		this.metrics.incrementMessageCount("nsfw");
 
@@ -41,8 +52,12 @@ public class NSFWMessageEventListener implements ApplicationListener<NSFWUploadE
 			this.publisher.publishEvent(
 					new UserBlockedEvent(this, user.getUsername(), "You have been blocked for uploading NSFW content.")
 			);
+
+			// Since the user is blocked, we need to disconnect them from the chat.
+			this.sessionService.deleteAllSessionsOfUser(user.getUsername());
 		}
 
 		this.userService.saveUser(user);
+		this.lock.unlock();
 	}
 }
